e54ebdc298082049729125145b20318899f4f2b1
VladGrygola <vladgrygola@gmail.com>
2020-04-18 20:52:51 +0300

    created app

--- src/App.js
+++ src/App.js
@@ -1,82 +1,64 @@
 /* eslint-disable no-extend-native */
 import React, { useState } from 'react';
+import NodeRSA from 'node-rsa';
 import './App.scss';
 
-const alphabet = `а, б, в, г, ґ, д, е, є, ж, з, и, і, ї, й, к, л, м, н, о, п, р, с, т, у, ф, х, ц, ч, ш, щ, ь, ю, я,  `;
-
-const originalAlphabetArray = alphabet.split(', ');
-
-let [a, b, c] = originalAlphabetArray;
-
-const encryptedAlphabetArray = [...originalAlphabetArray];
-
-encryptedAlphabetArray.shift();
-encryptedAlphabetArray.shift();
-encryptedAlphabetArray.shift();
-encryptedAlphabetArray.push(a);
-encryptedAlphabetArray.push(b);
-encryptedAlphabetArray.push(c);
-
-const enctyptChar = (c) => {
-  const id = originalAlphabetArray.findIndex((e) => e === c);
-  return encryptedAlphabetArray[id];
-};
-
-const dectyptChar = (c) => {
-  const id = encryptedAlphabetArray.findIndex((e) => e === c);
-  return originalAlphabetArray[id];
-};
-
-const encryptString = (s) =>
-  s
-    .split('')
-    .map((c) => enctyptChar(c))
-    .join('');
-const decryptString = (s) =>
-  s
-    .split('')
-    .map((c) => dectyptChar(c))
-    .join('');
-
 function App() {
+  console.clear();
+
   const [sourceText, setSourceText] = useState('');
-  const [idVisibleError, setIdVisibleError] = useState('');
+  const [isVisibleError, setIsVisibleError] = useState(false);
+  const [key, setKey] = useState(new NodeRSA({ b: 512 }));
 
   const handleDecryptInput = ({ target: { value } }) => {
-    value.split('').some((e) => !originalAlphabetArray.includes(e))
-      ? setIdVisibleError(true)
-      : setIdVisibleError(false);
-    setSourceText(value.toLowerCase());
+    setSourceText(value);
   };
 
   const handleEncryptInput = ({ target: { value } }) => {
-    value.split('').some((e) => !originalAlphabetArray.includes(e))
-      ? setIdVisibleError(true)
-      : setIdVisibleError(false);
-    setSourceText(decryptString(value));
+    try {
+      setSourceText(key.decrypt(value, 'utf8'));
+      setIsVisibleError(false);
+    } catch (e) {
+      setIsVisibleError(true);
+    }
   };
 
   return (
     <div className='App'>
       <div>
+        <div className='input-small '>
+          <span>Приватний ключ</span>
+          <textarea
+            className='private'
+            value={key.exportKey('private')}
+            disabled
+          />
+        </div>
+        <div className='input-small '>
+          <span>Публічний ключ</span>
+          <textarea
+            className='public'
+            value={key.exportKey('public')}
+            disabled
+          />
+        </div>
         <div className='input'>
           <span>Розшифрований текст</span>
           <input type='text' onChange={handleDecryptInput} value={sourceText} />
         </div>
         <div className='input'>
-          <span>Зашифрований текст текст</span>
-          <input
-            type='text'
+          <span>Зашифрований текст</span>
+          <textarea
             onChange={handleEncryptInput}
-            value={encryptString(sourceText)}
+            value={key.encrypt(sourceText, 'base64')}
           />
         </div>
+        {isVisibleError ? (
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <i>Некоректний ключ</i>
+          </div>
+        ) : null}
       </div>
-      {idVisibleError ? (
-        <div>
-          <i>Вводьте лише букви українстького алфавіту</i>
-        </div>
-      ) : null}
     </div>
   );
 }
